a71d9ec2c3d8536faf4bb4495a570f28041a5c93
gustavo.caruso <gustavoalmeida26@hotmail.com>
2022-02-17 15:10:22 -0300

    plus one

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const err = require('./layers/middlewares/error');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 app.use(router);
 app.use(err);
+app.use(cors({
+  origin: 'https://ebytr-cc-front.vercel.app/' || 'http://localhost:3000',
+  methods: ['POST', 'GET', 'PUT', 'DELETE']
+}));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`On at ${PORT}!`))
