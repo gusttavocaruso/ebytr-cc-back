c220c0b45c7efd4e839c56ed4411ee522699562d
gustavo.caruso <gustavoalmeida26@hotmail.com>
2022-02-17 16:13:22 -0300

    change in corsPermissions

--- layers/middlewares/corsPermission.js
+++ layers/middlewares/corsPermission.js
@@ -1,4 +1,4 @@
 module.exports = ((_req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", 'https://ebytr-cc-front.vercel.app/');
   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
 });
