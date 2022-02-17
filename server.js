a3651ba6dc9cfe0d4b11804114fde42282cf706a
gustavo.caruso <gustavoalmeida26@hotmail.com>
2022-02-17 16:50:53 -0300

    try again

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const router = require('./routes');
 const err = require('./layers/middlewares/error');
-// const corsAll = require('./layers/middlewares/corsPermission');
+const corsAllw = require('./layers/middlewares/corsPermission');
 
 const app = express();
 app.use(express.json());
-app.use((_req, res, next) => {
-  res.header("Access-Control-Allow-Origin", 'https://ebytr-cc-front.vercel.app');
-  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-  app.use(cors());
-  next();
-});
-// app.use(corsAll);
+app.use(corsAllw, cors());
+// app.use((_req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", 'https://ebytr-cc-front.vercel.app');
+//   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+//   app.use(cors());
+//   next();
+// });
+
 // app.use(cors({
 //   origin: 'https://ebytr-cc-front.vercel.app/',
 //   methods: ['POST', 'GET', 'PUT', 'DELETE']
