87988d12325b0d34aee404e0aa8248729dfd7999
gustavo.caruso <gustavoalmeida26@hotmail.com>
2022-02-17 18:57:39 -0300

    up

--- server.js
+++ server.js
@@ -7,19 +7,22 @@ const corsAllw = require('./layers/middlewares/corsPermission');
 const app = express();
 app.use(express.json());
 app.use(corsAllw, cors());
-// app.use((_req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", 'https://ebytr-cc-front.vercel.app');
-//   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-//   app.use(cors());
-//   next();
-// });
-
-// app.use(cors({
-//   origin: 'https://ebytr-cc-front.vercel.app/',
-//   methods: ['POST', 'GET', 'PUT', 'DELETE']
-// }));
 app.use(router);
 app.use(err);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`On at ${PORT}!`))
+
+/*
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", 'https://ebytr-cc-front.vercel.app');
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
+
+app.use(cors({
+  origin: 'https://ebytr-cc-front.vercel.app/',
+  methods: ['POST', 'GET', 'PUT', 'DELETE']
+}));
+*/
